59b0b766364adfd74e8b2c7d6413863fceafe213
Rahul Lal <Rahul21Lal@gmail.com>
2016-08-18 19:52:32 +1200

    Most of the files uploaded

---
+++ Controllers/ListingController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TradeMeSample.Models;
+
+namespace TradeMeSample.Controllers
+{
+    public class ListingController : Controller
+    {
+        private static readonly ListingViewModel[] Listings =
+        {
+            new ListingViewModel {id = 1, name = "My Ferrari", StartDate = DateTime.Now, Featured = false, Price = 50000 },
+            new ListingViewModel {id = 2, name = "My House", StartDate = DateTime.Now, Featured = true, Price = 500000 }
+        };
+
+
+        // GET: Listing
+        public ActionResult Index()
+        {
+            return View(Listings);
+        }
+
+        // GET: Listing/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Listing/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Listing/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Listing/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var modelToEdit = Listings.Single(model => model.id == id);
+            return View(modelToEdit);
+        }
+
+        // POST: Listing/Edit/5
+        [HttpPost]
+        public ActionResult Edit(LoginViewModel model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Listing/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Listing/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/api/ListingsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+using TradeMeSample.Models;
+
+namespace TradeMeSample.Controllers.api
+{
+    public class ListingsController : ApiController
+    {
+        private static readonly ListingViewModel[] Listings =
+        {
+                new ListingViewModel { id = 1, name = "My Ferrari", StartDate = DateTime.Now, Featured = false, Price = 50000 },
+                new ListingViewModel { id = 2, name = "My House", StartDate = DateTime.Now, Featured = true, Price = 500000 }
+        };
+
+        protected void Application_Start()
+        {
+
+        }
+
+        // GET: api/Listings
+        public IEnumerable<ListingViewModel> Get()
+        {
+            return Listings;
+        }
+
+        // GET: api/Listings/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Listings
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Listings/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Listings/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+}
